1de35b97e6a6c43ccbd7dc277fc6747e7e1738dd
Aswanidev-vs <apploginer123@gmail.com>
2025-07-06 13:15:15 +0530

    # changes made to logout.php

--- logout.php
+++ logout.php
@@ -11,16 +11,6 @@ session_start();
 
 // Save email to use in localStorage cleanup
 $email = isset($_SESSION['email']) ? $_SESSION['email'] : null;
-
-// Destroy session completely
-$_SESSION = array();
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
 session_destroy();
 ?>
 
